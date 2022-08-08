2d9cdc4ac9e2893a6d57da72d1304d17c1b4083c
Mouadh Aouiti <a.mouadh@emerya.fr>
2022-08-08 03:25:56 +0200

    gitignore correction

---
+++ wp-content/themes/Mouadh/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Main tempalte file
+ *
+ *
+ * @package Mouadh
+ */
+?>
+<!doctype html> 
+<html lang="en">
+<head>
+</head>
+
+<p>Hello WordPress theme </p>
+
+<body>
+<header>This is my Header</header>
+<footer>This is my footer</footer>
+</body>
+</html>
